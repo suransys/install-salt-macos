b61bcd7b2a01313e0e2bfdb0ee0e9d4edba7bb2c
Alex Clay <aclay@mac.com>
2021-04-23 09:34:53 -0400

    Initial commit to github

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # install-salt-macos
-Bash script to install saltstack on macOS
+Bash script to install saltstack on macOS using the official salt repo installer.
+
+## Usage
+
+```
+install_salt_macos.sh <salt version number> [user] [install homebrew] [archived version]
+```
+
+## Parameters
+| Parameter | Accepted Value | Use |
+| --------- | -------------- | --- |
+| salt version number | integer | version to install, i.e. 3003 |
+| (optional) user | user name | user who should install salt, defaults to current |
+| (optional) install homebrew | 0 for yes; anything else for no | should homebrew also be installed, defaults to yes |
+| (optional) archived version | 0 for yes; anything else for no | download from the salt archive, defaults to no |
+
+## Contributing
+Please see our [guide to contributing](https://github.com/suransys/contributing).
---
+++ install_salt_macos.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -eu
+
+SALT_VERSION="$1"
+SALT_USER="${2:-$(whoami)}"
+INSTALL_HOMEBREW="${3:-0}"
+ARCHIVED_VERSION="${4:-1}"
+
+if [ -e /opt/salt/bin/salt-call ]; then
+	CURRENT_SALT_VERSION=$(/opt/salt/bin/salt-call --version | grep -E -o '\d+')
+	if (( $(echo "${CURRENT_SALT_VERSION} == ${SALT_VERSION}" | bc -l) )); then
+		echo "salt-call is available and returning ${SALT_VERSION}"
+		exit 0
+	fi
+
+	if (( $(echo "${CURRENT_SALT_VERSION} > ${SALT_VERSION}" | bc -l) )); then
+		echo "salt-call is available and returning ${CURRENT_SALT_VERSION}; will not downgrade to ${SALT_VERSION}"
+		exit 0
+	fi
+fi
+
+echo "install_salt_macos.sh will install ${SALT_VERSION}-py3"
+
+if [ $"{ARCHIVED_VERSION}" -eq 0 ]; then
+	REPO_URL="https://archive.repo.saltproject.io"
+else
+	REPO_URL="https://repo.saltproject.io"
+fi
+
+PKG_NAME="salt-${SALT_VERSION}-py3-x86_64.pkg"
+TEMP_DIR="$(mktemp -d)"
+PKG_FILE="${TEMP_DIR}/${PKG_NAME}"
+URL="${REPO_URL}/osx/${PKG_NAME}"
+
+curl -sSL -o "${PKG_FILE}" "${URL}"
+sudo installer -pkg "${PKG_FILE}" -target /
+rm "${PKG_FILE}"
+rm "${TEMP_DIR}"
+
+# We need Homebrew for salt pkg management
+if [ "${INSTALL_HOMEBREW}" -eq 0 ]; then
+	echo "Fetching Homebrew core as a non-shallow copy as ${SALT_USER}..."
+	su - "${SALT_USER}" -c 'git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core" fetch --unshallow' || true
+    
+	echo "Installing Homebrew as ${SALT_USER}..."
+	su - "${SALT_USER}" -c '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null'
+    
+	echo "Updating Homebrew as ${SALT_USER}..."
+	su - "${SALT_USER}" -c '/usr/local/bin/brew update'
+    
+	echo "Tapping services"
+	su - "${SALT_USER}" -c '/usr/local/bin/brew tap homebrew/services'
+
+	# Ensure homebrew is in the ssh user's path
+	echo "Adding Homebrew to the ${SALT_USER} user's path..."
+	su - "${SALT_USER}" -c "echo 'export PATH=/usr/local/bin:/usr/local/sbin:\$PATH' >> .bash_profile && chmod 0700 .bash_profile"
+
+	# Writing to .bashrc allows ssh clients to load this path
+	# such as when running kitchen salt
+	su - "${SALT_USER}" -c "echo 'export PATH=/usr/local/bin:/usr/local/sbin:\$PATH' >> .bashrc && chmod 0700 .bashrc"
+fi
+
+echo "macOS config complete"
+
+exit 0
